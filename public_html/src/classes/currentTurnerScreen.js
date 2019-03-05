977b007fd125543692297316009a59b3fbecc3d3
YoussefAlmardini <yosefmardini@gmail.com>
2019-03-05 10:58:27 +0100

    turnerScreenAdded

--- public_html/src/classes/currentTurnerScreen.js
+++ public_html/src/classes/currentTurnerScreen.js
@@ -1,6 +1,17 @@
 export default class CurrentTurnerScreen{
 
-  constructor(turner_name,E_score,D_score,N_score,totalScore){
+  constructor(turner_name,turner_number,E_score,D_score,N_score,totalScore){
+
+    this.turner_name = turner_name;
+    this.turner_number =turner_number;
+    this.E_score = E_score;
+    this.D_score =D_score;
+    this.N_score = N_score;
+    this.totaalScore = totalScore;
+
+  }
+
+  Update = function () {
 
   }
 
