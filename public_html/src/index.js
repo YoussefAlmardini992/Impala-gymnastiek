b800163e3240b74d4ad78854075a61b08017184a
YoussefAlmardini <yosefmardini@gmail.com>
2019-03-05 12:47:23 +0100

    js src classes added

--- public_html/src/index.js
+++ public_html/src/index.js
@@ -3,6 +3,7 @@ import Groep from './classes/groep.js';
 import Secretariaat from "./classes/secretariaat.js";
 import ScoreBoard from "./classes/ScoreBoard.js";
 import CurrentTurnerScreen from "./classes/currentTurnerScreen.js";
+import Wedstrijd from "./classes/wedstrijd.js";
 
 const turner1 = new Turner('rawand', 'Ras', 'Shakir', 'man');
 const turner2 = new Turner('maya', 'van', 'lopijn', 'vrouw');
@@ -23,6 +24,11 @@ groep1.delete(turner2);
 console.log(groep1);
 console.log(groep1.getTurner('rawand'));
 const ScoreBoard1 = new ScoreBoard(groep1.groep_naam, groep1.turners);
-const CurrentTurnerScreen1 = new CurrentTurnerScreen(groep1.getTurner("rawand").voornaam,10,10,10,0);
+
+const liveWedStrijd = new Wedstrijd(groep1,[1,2,3]);
+
+const CurrentTurnerScreen1 = new CurrentTurnerScreen(liveWedStrijd.current_Turner,10,10,10,0);
 
 console.log(ScoreBoard1);
+console.log(liveWedStrijd);
+console.log(CurrentTurnerScreen1);
