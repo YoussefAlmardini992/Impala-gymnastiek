d81680ffa84203b9751ebd14488911519c843987
TLSvdAvoort <somegamemusic@gmail.com>
2019-03-05 10:53:50 +0100

    OverzichtSecretariaat

--- public_html/pages/secretariaatOverzicht.php
+++ public_html/pages/secretariaatOverzicht.php
@@ -1,7 +1,39 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Metamorphosis
- * Date: 04/03/2019
- * Time: 13:18
- */
+
+?>
+<html>
+<head>
+    <title>
+        Impala - Secretariaat
+    </title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:700" rel="stylesheet">
+    <link rel="stylesheet" href="../styles/overzichtStyles.css">
+</head>
+<body>
+    <div id="mySidenav" class="sidenav">
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+    <a href="#">Turners</a>
+    <a href="#">Groepen</a>
+    <a href="#">Wedstrijden</a>
+    <a href="#">LIVE</a>
+    <a>Log uit</a>
+    </div>
+
+    <div id="main">
+        <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; Open</span>
+        <h2>Secretariaat</h2>
+    </div>
+    <script src="src/index.js" type="module"></script>
+</body>
+<script>
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+</script>
+</html>
