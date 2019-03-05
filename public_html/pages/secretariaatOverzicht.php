ec7dc38140f738a6d2c9df330dd7641d87de51a5
Rawand_shakir <rwnd.shakir@gmail.com>
2019-03-05 12:52:28 +0100

    layout turners

--- public_html/pages/secretariaatOverzicht.php
+++ public_html/pages/secretariaatOverzicht.php
@@ -9,8 +9,10 @@ if(!isset($_SESSION["id"]) && $_SESSION["id"] != "secretariaat"){
     <title>
         Impala - Secretariaat
     </title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:700" rel="stylesheet">
     <link rel="stylesheet" href="../styles/overzichtStyles.css">
+    <link rel="stylesheet" href="../styles/turnersoverzicht.css">
 </head>
 <body>
     <div id="mySidenav" class="sidenav">
@@ -25,6 +27,9 @@ if(!isset($_SESSION["id"]) && $_SESSION["id"] != "secretariaat"){
     <div id="main">
         <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; Open</span>
         <h2>Secretariaat</h2>
+        <div class="overzichtContainer">
+            <?php require_once('../overzichten/turners.php');?>
+        </div>
     </div>
     <script src="src/index.js" type="module"></script>
 </body>
