d58afffedd207228dd2aad9a306909714ade462b
Rawand_shakir <rwnd.shakir@gmail.com>
2019-03-19 14:59:42 +0100

    log in value to server.js

--- public_html/pages/juryOverzicht.php
+++ public_html/pages/juryOverzicht.php
@@ -4,7 +4,7 @@ session_start();
 if(!isset($_SESSION["id"]) && $_SESSION["id"] != "jury"){
     header('Location: ../index.php');
 } else {
-
+        $loginID = $_SESSION["id"];
 }
 ?>
 <html>
@@ -15,10 +15,11 @@ if(!isset($_SESSION["id"]) && $_SESSION["id"] != "jury"){
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:700" rel="stylesheet">
     <link rel="stylesheet" href="../styles/overzichtStyles.css">
     <link rel="stylesheet" href="../styles/juryOverzicht.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.2.0/socket.io.dev.js"></script>
 </head>
 <body>
 <div id="main">
-    <a class="score-logout" href="../uti/logout.php">X</a>
+    <a class="score-logout" href="../uti/logout.php" onclick="ClearLoginValue();">X</a>
     <div class="header">
 
         <div class="item" style="text-align: left">
@@ -64,6 +65,18 @@ if(!isset($_SESSION["id"]) && $_SESSION["id"] != "jury"){
         input.value = 10;
       }
     }
+    let value = "<?php echo $loginID; ?>";
+    const socket = io.connect('http://localhost:3000');
+    socket.emit('Login_value',value);
+
+    function ClearLoginValue() {
+        x = null;
+        socket.emit('Login_value',value);
+    }
+    socket.on('get_jury',function (res) {
+        console.log(res);
+    })
+
 </script>
 </html>
 
--- public_html/src/server.js
+++ public_html/src/server.js
@@ -1,23 +1,23 @@
 const socket = require('socket.io'),
-      express = require('express'),
-      https = require('https'),
-      http = require('http'),
-      logger = require('winston'),
-      mysql = require('mysql');
+    express = require('express'),
+    https = require('https'),
+    http = require('http'),
+    logger = require('winston'),
+    mysql = require('mysql');
 
 let connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'rocle_db10'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'rocle_db10'
 });
 
-connection.connect(function(err) {
-  if (err) {
-    return console.error('error: ' + err.message);
-  }
+connection.connect(function (err) {
+    if (err) {
+        return console.error('error: ' + err.message);
+    }
 
-  console.log('Connected to the MySQL server.');
+    console.log('Connected to the MySQL server.');
 });
 
 
@@ -28,23 +28,31 @@ logger.info('Socket connected...');
 
 function emitConnection(SERVER) {
 
-  const io = socket.listen(SERVER);
+    const io = socket.listen(SERVER);
 
-  io.sockets.on('connection',function (socket) {
+    io.sockets.on('connection', function (socket) {
 
-    //Socket Orders
-    //ON SELECT GROUP
-    socket.on('select_group',function (groupName) {
+        //Socket Orders
+        //ON SELECT GROUP
+        socket.on('select_group', function (groupName) {
 
-      console.log(groupName);
+            console.log(groupName);
 
-      connection.query('SELECT * FROM deelnemers', function (error, results, fields) {
-        if (error) throw error;
-        console.log('The solution is: ', results[0]);
-        socket.emit('selected_group',results[0])
-      });
+            connection.query('SELECT * FROM deelnemers', function (error, results, fields) {
+                if (error) throw error;
+                console.log('The solution is: ', results[0]);
+                socket.emit('selected_group', results[0])
+            });
+        });
+
+        //ON SELECT JURY
+        socket.on('Login_value', function (value) {
+            socket.emit('get_jury',value);
+        });
+
     })
-  })
+
+
 }
 
 emitConnection(http_server);
