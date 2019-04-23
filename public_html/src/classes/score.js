cc65c07776fa7562cc20442a37b7e4a0bf7b08b9
YoussefAlmardini <yosefmardini@gmail.com>
2019-04-23 12:34:29 +0200

    add score class

---
+++ public_html/src/classes/score.js
@@ -0,0 +1,16 @@
+class Score {
+
+  constructor(d_Score, e_Score, n_Score, onderdeel, nummer, totaal) {
+
+    this.D = d_Score;
+    this.E = e_Score;
+    this.Onderdeel = onderdeel
+    this.N = n_Score;
+    this.Nummer = nummer;
+    this.Total = totaal;
+    this.deelnemer_ID = null;
+    this.wedstrijd_ID = null;
+    this.onderdeel_id = null;
+    this.subonderdeel_id = null;
+  };
+}
